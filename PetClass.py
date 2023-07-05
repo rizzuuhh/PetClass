bcce1f1e63b5a6915fb797b6ef7f8c0c2fd11f2c
rizza <denostarizza43@gmail.com>
2023-07-05 18:41:38 +1200

    Pseudocode

---
+++ PetClass.py
@@ -0,0 +1,11 @@
+# Initialize data attributes
+# Set the name of pet
+# Set the animal type of the pet
+# Set the age of the page
+# Return the pet's name
+# Return the pet's animal type
+# Return the pet's age
+# Create a Pet object
+# Prompt the user for the details of the pet
+# Store the entered details as object attributes
+# Retrieve and display the details of the pet using accessor methods
