ab3f176b234230a75ad0d4b28657e3daf1dbeda0
rizza <denostarizza43@gmail.com>
2023-07-05 19:03:06 +1200

    set pet's name

--- PetClass.py
+++ PetClass.py
@@ -5,9 +5,13 @@ class Pet:
      self.__animal_type = ""
      self.__age = 0
 
-# Set the name of pet
-# Set the animal type of the pet
-# Set the age of the page
+     # Set the name of pet
+    def set_name(self, name):
+     self.__name = name
+
+     # Set the animal type of the pet
+
+     # Set the age of the page
 # Return the pet's name
 # Return the pet's animal type
 # Return the pet's age
